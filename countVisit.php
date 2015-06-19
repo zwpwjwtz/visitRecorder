129e65e79bc76fa7d4ad01cc0c69cc3213e1f20d
zwpwjwtz <zwpwjwtz@126.com>
2015-06-19 13:04:00 +0800

    Add support of log splitting

--- countVisit.php
+++ countVisit.php
@@ -6,9 +6,76 @@
  * 版权所有 zwpwjwtz(at)163.com 2006-2015。
 **/
 
-define('VISIT_MAX_CACHE',20);	//最大缓存IP数
 define('VISIT_LOG_PATH','../');	//访问记录的存储路径
+define('MAX_LOG_SIZE',100000);  //单个日志文件的最大尺寸
+define('LOG_NAME_FORMAT','visitRecord_%03d.txt');    //日志文件的命名格式(用于sprintf)
+
+define('VISIT_MAX_CACHE',20);	//最大缓存IP数
 define('VISIT_CACHE_TIME',3600);	//在多长的时间内认为来自同一IP的访问是相同的
+
+
+function getLogFileName($logNumber)
+{
+    return VISIT_LOG_PATH.sprintf(LOG_NAME_FORMAT,$logNumber);
+}
+
+function getMaxLogNumber()
+{
+    //查找首次连续出现的文件序号的最大值
+    //若存在序号不连续的文件，可能导致判断错误！
+    $number=0;
+    $default=getLogFileName($number);
+    $probe=$default;
+    
+    //查找最小编号
+    while(!file_exists($probe))
+    {
+        if ($probe==$default && $number!=0) break;
+        $number++;
+        $probe=getLogFileName($number);
+    }
+    
+    //查找最大编号
+    $last=$number;
+    while(file_exists($probe))
+    {
+        if ($probe==$default && $number!=0) break;
+        $last=$number++;
+        $probe=getLogFileName($number);
+    }
+    
+    return $last;
+}
+
+function correctFileSize($filename,$number)
+{
+    if (filesize($filename) > MAX_LOG_SIZE)
+    {
+        $n=(int)(filesize($filename) / MAX_LOG_SIZE);
+        $fp=fopen($filename,'rb');
+        $firstPart='';
+        for($i=0;$i<filesize($filename);$i+=MAX_LOG_SIZE)
+        {
+            fseek($fp,$i);
+            $temp=fread($fp,MAX_LOG_SIZE);
+            
+            //避免将一个记录分割开来
+            $p=strrpos($temp,"\n");
+            $i-=strlen($temp)-$p-1;
+            $temp=substr($temp,0,$p);
+            
+            if ($firstPart=='')
+                $firstPart=$temp;
+            else
+                file_put_contents(getLogFileName($number),$temp);
+            
+            $number++;
+        }
+        fclose($fp);
+        file_put_contents($filename,$firstPart);
+    }
+}
+
 
 function countVisit()
 { 
@@ -19,22 +86,44 @@ function countVisit()
 		$addr = ($_SERVER['HTTP_VIA']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 		if ($addr=='unknown' || $addr=='' || !isCached($addr))
 		{
-			$f=fopen(VISIT_LOG_PATH.'visitRecord.txt','a');
+                        //准备写入的记录
+                        $addr=$addr.','.date('Y-m-d H:i:s')."\n";
+                        
+                        //检查日志文件尺寸
+                        $log_number=getMaxLogNumber();
+                        $log_file=getLogFileName($log_number);
+                        if (!file_exists($log_file))
+                        {
+                            file_put_contents($log_file,NULL);
+                        }
+                        else
+                        {
+                            correctFileSize($log_file,$log_number,LOG_NAME_FORMAT);
+                            $log_number=getMaxLogNumber();
+                            $log_file=getLogFileName($log_number);
+                        }                        
+                        if (filesize($log_file)+strlen($addr) > MAX_LOG_SIZE)
+                        {
+                            $log_number++;
+                            $log_file=getLogFileName($log_number);
+                        }
+                        
+                        //写入日志文件
+			$f=fopen($log_file,'a');
   			if ($f)
   			{
 				flock($f,LOCK_EX);
-  				fwrite($f,$addr.','.date('Y-m-d H:i:s')."\n");
+  				fwrite($f,$addr);
    				flock($f,LOCK_UN);
 			}	
    			fclose($f);
 		}
 	}
-}
+}        
 
 function isCached($ip)
 {
-	$cache_file=VISIT_LOG_PATH.'visitCache';
-	if (!file_exists($cache_file)) file_put_contents($cache_file,NULL);
+	$cache_file=VISIT_LOG_PATH.'visitCache';        
 	$cachedIP=file_get_contents($cache_file);
 	if (strstr($cachedIP,$ip))//匹配到完整的IP记录
 	{
